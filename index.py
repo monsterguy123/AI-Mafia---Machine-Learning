11125669080b58272f4c8d0ee7d3618bf9ff5377
Deepak Bisht <deepakbisht9891@gmail.com>
2025-10-22 20:12:27 +0530

    first commit

--- index.py
+++ index.py
@@ -1,8 +1,20 @@
-Str = "1 2 3 4 5".split(" ")
-New_List = []
-for num in Str:
-   New_List.append(int(num))
+import numpy as np
+print(np.__version__)
 
-print(New_List)
+arr = np.array([[-1,2,0,4],
+                [4,-0.5,6,0],
+                [2.6,0,7,8],
+                [3,-7,4,2.0]])
 
-print("The capital of %s is %s is numbered %d" % ("India","Delhi",1))
+# print("No of Dimension..." , arr.ndim)
+# print("Shape of array..." , arr.shape)
+
+# print(arr*2)
+
+# print(arr.T)
+
+print("Max element in the array" , arr.max())
+print("Max element in the array col-wise" , arr.max(axis=0))
+print("Max element in the array row-wise" , arr.max(axis=1))
+np.sort(arr,axis=1)
+print("sort element in the array" ,arr)
