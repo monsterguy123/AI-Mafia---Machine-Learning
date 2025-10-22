37cdf5c48c6b8e26f42cbe91a9c50a04b496dd7c
Deepak Bisht <deepakbisht9891@gmail.com>
2025-10-22 13:57:35 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,8 @@
+Str = "1 2 3 4 5".split(" ")
+New_List = []
+for num in Str:
+   New_List.append(int(num))
+
+print(New_List)
+
+print("The capital of %s is %s is numbered %d" % ("India","Delhi",1))
