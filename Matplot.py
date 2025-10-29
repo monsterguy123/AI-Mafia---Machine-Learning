04786fb1a346c063923dc770c3b634cd6ced037b
Deepak Bisht <deepakbisht9891@gmail.com>
2025-10-29 14:07:37 +0530

    Email Sending

---
+++ Matplot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+# x = [1, 5, 8,12,7,2]
+# y = [1, 5, 8,2,7,2]
+
+# plt.style.use('fivethirtyeight')
+
+# # Use plt.plot instead of plt.show for plotting
+# plt.plot(x, y, color='green', linestyle='dashed', linewidth=3,
+#          marker='o', markerfacecolor='red', markersize=8)
+
+# plt.show()
+# ---------------------------------#---------------------------------#
+# plotting bar chart
+# left = [1 ,2, 3, 4, 5]
+
+# height = [10, 24, 36, 40, 5]
+
+# tick_label = ['one','two','three','four','five']
+
+# plt.bar(left,height,tick_label=tick_label,width=0.8,color = ['red','blue'])
+
+# plt.xlabel('x-label')
+# plt.ylabel('y-label')
+
+# plt.title('My Bar Chart!')
+# plt.show()
+# plt.close()
+
+# ---------------------------------#---------------------------------#
+#plot histogram
